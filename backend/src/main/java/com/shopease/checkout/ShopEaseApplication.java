96b33de397b1d532428c097508ece1c9ea38fd08
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2026-03-31 12:07:46 +0000

    Enable @EnableAsync in ShopEaseApplication to support asynchronous processing

--- backend/src/main/java/com/shopease/checkout/ShopEaseApplication.java
+++ backend/src/main/java/com/shopease/checkout/ShopEaseApplication.java
@@ -2,8 +2,10 @@ package com.shopease.checkout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ShopEaseApplication {
 
     public static void main(String[] args) {
